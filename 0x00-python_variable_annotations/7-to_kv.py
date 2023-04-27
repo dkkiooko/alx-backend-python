cbac8c02608bfcff2de087a591a372d8ed85ad19
dkkiooko <kithekadankioko@gmail.com>
2023-04-27 23:00:04 +0300

    used typing Tuple for the return value

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """type-annotated function
 """
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """tuple of int and float"""
-    return tuple([k, v])
+    return (k, v**2)
