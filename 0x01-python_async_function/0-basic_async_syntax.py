ef3a250c53d382f3955ab979e95b67ad22cd3fa1
dkkiooko <kithekadankioko@gmail.com>
2023-05-03 16:22:25 +0300

    returned delay instead of the max_delay

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,4 +11,4 @@ async def wait_random(max_delay: int = 10) -> int:
     waits for a random delay between 0 and max_delay"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
-    return max_delay
+    return delay
