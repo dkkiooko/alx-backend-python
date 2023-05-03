06a2f161e46030808b185fbc0056a9db303c0a08
dkkiooko <kithekadankioko@gmail.com>
2023-05-03 16:24:12 +0300

    fixed the return type from int to float

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,8 +6,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10) -> int:
-    """ returns int
+async def wait_random(max_delay: int = 10) -> float:
+    """ returns delay
     waits for a random delay between 0 and max_delay"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
