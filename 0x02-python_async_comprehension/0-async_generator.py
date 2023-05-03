4263d2d9aad332fd3c62f5b2865cedc147deb569
dkkiooko <kithekadankioko@gmail.com>
2023-05-03 20:12:32 +0300

    generate async iterable

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""coroutine generator"""
+import asyncio
+import random
+import typing
+
+
+async def async_generator() -> typing.Generator[float, None, None]:
+    """async generator """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
