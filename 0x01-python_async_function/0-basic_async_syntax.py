c0b3c84ce92bf2efd5d8aa799a55b310ec0271fd
dkkiooko <kithekadankioko@gmail.com>
2023-05-03 16:21:10 +0300

    async function

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""learn async python
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> int:
+    """ returns int
+    waits for a random delay between 0 and max_delay"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return max_delay
