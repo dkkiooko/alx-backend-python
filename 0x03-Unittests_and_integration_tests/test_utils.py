80ce08358c28bcd9bfb8287f1d182c33ed29579b
dkkiooko <kithekadankioko@gmail.com>
2023-05-24 16:03:55 +0300

    mock http calls using patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """create a unittest for a utils function"""
+import requests
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import (access_nested_map, get_json)
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -29,3 +31,19 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
         self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    """ mock HTTP calls """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """test utils.get_json """
+        attr = {'json.return_value': test_payload}
+        patcher = patch('requests.get', return_value=Mock(**attr))
+        mock = patcher.start()
+        self.assertEqual(get_json(test_url), test_payload)
+        mock.assert_called_once()
+        patcher.stop()
