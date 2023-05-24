26ce2857a46301aa1f2f3e78da616ac36ba08d83
dkkiooko <kithekadankioko@gmail.com>
2023-05-24 23:56:58 +0300

    test client module get_json function

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ test module """
 import unittest
+from unittest import mock
 from client import GithubOrgClient
 from unittest.mock import (
     MagicMock,
@@ -12,13 +13,13 @@ from parameterized import parameterized
 class TestGithubOrgClient(unittest.TestCase):
     """ tests the `GithubOrgClient` class """
     @parameterized.expand([
-        ("google", {'login': "google"}),
-        ("abc", {'login': "abc"})
+        ("google"),
+        ("abc")
     ])
     @patch("client.get_json")
-    def test_org(self, org, resp, mock):
+    def test_org(self, org, mock):
         """ test the org method """
-        mock = MagicMock(return_value=resp)
-        client = GithubOrgClient(org)
-        self.assertEqual(client.org(), resp)
-        mock.assert_called_once_with(f"https://api.github.com/orgs/{org}")
+        url = 'https://api.github.com/orgs{}'.format(org)
+        spec = GithubOrgClient(org)
+        spec.org()
+        mock.assert_called_once_with(url)
